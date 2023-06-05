7d4e21d32a8dba87357bcd16f3f50c6a3cd373b8
ahmetbahadircol <ahmetbahadircol@gmail.com>
2023-06-05 16:54:29 +0300

    epic games first commit

--- get_steam_price.py
+++ get_steam_price.py
@@ -7,7 +7,6 @@ def _prepare_request(game_term: str):
         'Accept': '*/*',
         'Accept-Language': 'en-US,en;q=0.9',
         'Connection': 'keep-alive',
-        # 'Cookie': 'steamCountry=TR%7C59fc48c1ab53191fc67be2bc1a73af20; browserid=3058553419775486659; sessionid=f0a710149b6672b9039de1ab; timezoneOffset=10800,0; _ga=GA1.2.1414314939.1685690741; _gid=GA1.2.1434797678.1685690741; recentapps=%7B%221326470%22%3A1685690745%7D; app_impressions=2292130@1_7_15__13|2292080@1_7_15__13|1316600@1_7_15__13|1040420@1_7_15__13',
         'Referer': 'https://store.steampowered.com/app/1326470/Sons_Of_The_Forest/',
         'Sec-Fetch-Dest': 'empty',
         'Sec-Fetch-Mode': 'cors',
